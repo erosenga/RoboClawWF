4614a7c833c9a28a3d1e7d5201b80d5ac3e51d13
Eliezer Rosengaus <erosengaus@gmail.com>
2023-11-06 14:27:45 -0800

    eanup

---
+++ RoboClawWF/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StepperWF
+{
+    public partial class Form1 : Form
+    {
+        public string CurrentMacro = "stepper.tst.txt";
+        public StepperController stepperController;
+        public Form1()
+        {
+
+            InitializeComponent();
+            button2.Text = CurrentMacro;
+            stepperController = new StepperController(CurrentMacro, this);
+
+        }
+        // run macro
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Control[] macro = this.Controls.Find("button2", true);
+            string CurrentMacro = macro[0].Text;
+            MacroRunner macroRunner = new MacroRunner(stepperController, CurrentMacro);
+            macroRunner.RunMacro();
+        }
+
+        // Select macro
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var picker = new OpenFileDialog();
+            picker.FileName = CurrentMacro;
+            picker.DefaultExt = "txt";
+            picker.InitialDirectory = Environment.CurrentDirectory;
+            picker.Filter = "txt files (*.txt)|*.txt";
+            if (picker.ShowDialog() == DialogResult.OK)
+            {
+                CurrentMacro = picker.FileName;
+                button2.Text = CurrentMacro;
+
+            }
+        }
+
+        public void SetStatus(string s)
+        {
+            button3.Text = s;
+        }
+
+    }
+}
---
+++ RoboClawWF/TimeUtils.cs
@@ -0,0 +1,32 @@
+﻿
+
+using System;
+
+namespace CommandMessenger
+{
+    /// <summary>Class to get a timestamp </summary>
+    public static class TimeUtils
+    {
+        public static readonly DateTime Jan1St1970 = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);  // 1 January 1970
+
+        /// <summary> Gets the milliseconds since 1 Jan 1970. </summary>
+        /// <value> The milliseconds since 1 Jan 1970. </value>
+        public static long Millis { get { return (long)((DateTime.Now.ToUniversalTime() - Jan1St1970).TotalMilliseconds); } }
+
+        /// <summary> Gets the seconds since 1 Jan 1970. </summary>
+        /// <value> The seconds since 1 Jan 1970. </value>
+        public static long Seconds { get { return (long)((DateTime.Now.ToUniversalTime() - Jan1St1970).TotalSeconds); } }
+
+        // Returns if it has been more than interval (in ms) ago. Used for periodic actions
+        public static bool HasExpired(ref long prevTime, long interval)
+        {
+            var millis = Millis;
+            if (millis - prevTime > interval)
+            {
+                prevTime = millis;
+                return true;
+            }
+            return false;
+        }
+    }
+}
